c77a7c185cb00a704076a805f3c6e65917e02877
Yannis Gkoumas <ygkoumas@gmail.com>
2016-12-28 19:16:50 +0000

    Remove print

--- __main__.py
+++ __main__.py
@@ -6,7 +6,6 @@ from os import system
 button = Button(3)
 
 def pressed(button):
-	print(str(button.pin.number) + ' GPIO pin triggered shutdown')
 	system('shutdown -h now')
 
 button.when_pressed = pressed
